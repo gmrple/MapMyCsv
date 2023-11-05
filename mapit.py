6f2452702d0b04480cbebed3bac899e5f02c6a60
Kevin Jansen <gmrple@gmail.com>
2023-11-04 21:36:32 -0700

    cleanup

--- mapit.py
+++ mapit.py
@@ -5,58 +5,38 @@ import sys
 
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QColor
-from qgis.core import *
+from qgis.core import QgsApplication, QgsProject, QgsCoordinateReferenceSystem, QgsVectorLayer, QgsRasterLayer, QgsMapSettings, QgsMapRendererParallelJob
 
 with open("./MapMyCsv.yaml", "r") as f:
-    print("opened config file")
     config = yaml.safe_load(f)
     elipsoid = config['settings']['elipsoid']
     csvPath = config['settings']['csvPath']
     
-    # Initiate Application
-    QgsApplication.setPrefixPath("/usr", True)
-    print("set prefix")
+    QgsApplication.setPrefixPath(config['settings']['prefixPath'], True)
     qgs = QgsApplication([], False)
     
     qgs.initQgis()
-    print("Application initiated")
-    
     wgs84="EPSG:4326"
 
-    # Start project instance
     project = QgsProject.instance()
-    #project.setCrs(QgsCoordinateReferenceSystem(elipsoid))
     project.setCrs(QgsCoordinateReferenceSystem(elipsoid))
 
-    # Build layer from CSV and check validity
+    # if you get a file imported as you want in the desktop application, you can hover over the layer
+    # to gett the settings that are in the URI
     uri = 'file://'+csvPath+"?type=csv&detectTypes=yes&crs="+wgs84+"&xField=longitude&yField=latitude&spatialIndex=no&subsetIndex=no"
-    print(uri)
     csvLayer = QgsVectorLayer(uri, 'GpsLocations', 'delimitedtext')
-
+    
+    # openstreetmap layer
     tms = 'type=xyz&url=https://tile.openstreetmap.org/{z}/{x}/{y}.png&zmax=19&zmin=0'
     osmLayer = QgsRasterLayer(tms,'OSM', 'wms')
 
-    if csvLayer.isValid():
-        print('CSV Layer successfully imported')
-    else:
-        print('CSV Import failed')
-    if osmLayer.isValid:
-        print('OpenStreetMaps layer imported')
-    else:
-        print('unable to make OpenStreetMaps Layer')
-        sys.exit(0)
-
-
-    # Adding map layers
     project.addMapLayer(osmLayer)
     project.addMapLayer(csvLayer)
-
-    # Set layer colors and create some outlines
+    
     csvSymbols = csvLayer.renderer().symbol()
     csvSymbols.setColor(QColor('red'))
     csvLayer.triggerRepaint()
 
-    # Set layer order and output options
     options = QgsMapSettings()
     options.setLayers([csvLayer,osmLayer])
     options.setBackgroundColor(QColor("transparent"))
@@ -64,20 +44,15 @@ with open("./MapMyCsv.yaml", "r") as f:
     options.setExtent(csvLayer.extent())
     options.setDestinationCrs(QgsCoordinateReferenceSystem(wgs84))
 
-    mapLayers = project.mapLayers()
-    for x, y in enumerate(mapLayers): print('Layer ' + str(x+1) + ': '+ str(y.split('_')[0]))
-
-    # Start image render
-
     render = QgsMapRendererParallelJob(options)
     image_location = './file.png'
     render.start()
     render.waitForFinished()
     img = render.renderedImage()
     img.save(image_location, 'png')
+    
     print('Map has been rendered to: ' + image_location)
+    project.write("project.qgz")
 
-
-    # End of project code
     qgs.exitQgis()
     print("Exiting")
